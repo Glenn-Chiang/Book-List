ed79c6f72e62732be3f282b79670e6ba156ef881
Glenn Chiang <glennchiang24@gmail.com>
2023-05-25 15:50:04 +0800

    Added Book factory function and library object. Clicking on submit button in add-book form will create a Book object from the form values and add the Book object into the library object.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const addBookBtn = document.querySelector('button.add-book');
 const addBookForm = document.querySelector('form.add-book');
-const submitAddBook = document.querySelector("form.add-book button[type='submit']")
+const submitAddBook = document.querySelector("form.add-book button.submit")
 const cancelAddBook = document.querySelector("form.add-book button.cancel")
 
 addBookBtn.addEventListener('click', () => {
@@ -12,4 +12,45 @@ cancelAddBook.addEventListener('click', () => {
     addBookForm.classList.remove('show');
     addBookBtn.classList.remove('hide');
 })
+
+
+// Book object factory function
+function Book(title, author, rating, dateRead) {
+    const fullTitle = `'${title}' by ${author}`;
+    return { title, author, rating, dateRead, fullTitle };
+}
+
+// Library object
+const library = {
+    books: [],
+    addBook: function (book) {
+        book.index = this.books.length + 1;
+        this.books.push(book);
+    },
+    listBooks: function () {
+        this.books.forEach((book) => {
+            console.log(book);
+        })
+    }
+}
+
+
+submitAddBook.addEventListener('click', () => {
+    const titleField = document.getElementById('title');
+    const authorField = document.getElementById('author');
+    const ratingField = document.getElementById('rating');
+    const dateReadField = document.getElementById('date-read');
+    
+    const title = titleField.value;
+    const author = authorField.value;
+    const rating = ratingField.value;
+    const dateRead = dateReadField.value;
+
+    const book = Book(title, author, rating, dateRead);
+    library.addBook(book);
+
+    alert(`Added ${book.fullTitle} to Library!`);
+})
+
+
 
